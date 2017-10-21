d716e8d073829f6d7454f218dca13f22a3c23e3c
Mahmoud333 <Mahmoud@mahmouds-mini.zte.com.cn>
2017-10-21 20:25:19 +0200

    Initial Commit

---
+++ Marvel Vezeeta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marvel Vezeeta
+//
+//  Created by Mahmoud Hamad on 10/21/17.
+//  Copyright © 2017 Mahmoud SMGL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Marvel VezeetaTests/Marvel_VezeetaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Marvel_VezeetaTests.swift
+//  Marvel VezeetaTests
+//
+//  Created by Mahmoud Hamad on 10/21/17.
+//  Copyright © 2017 Mahmoud SMGL. All rights reserved.
+//
+
+import XCTest
+@testable import Marvel_Vezeeta
+
+class Marvel_VezeetaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
